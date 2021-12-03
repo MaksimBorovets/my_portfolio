a6df5a5eec45680beb789a54c7705d31c934fc70
MaksimBorovets <maksimborovets1992@gmail.com>
2021-12-03 14:00:14 +0200

    add Bro | Wheels

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'ЯрКава',
     description: "Лэндинг кофейни с красивым адаптивным дизайном, призывающий посетителя зарегистрироваться, получить бонус и посмотреть на карте расположение кофеень.",
-      image: '/images/1.png',
+      image: '/images/1.jpg',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://wonderful-borg-f1e2df.netlify.app/',
     visit: 'https://wonderful-borg-f1e2df.netlify.app/',
@@ -11,10 +11,19 @@ export const projects = [
   {
     title: 'KULD Landscape Design',
     description:"Сайт-визитка компании, занимающейся автополивом газонов, озеленением и ландшафтным дизайном. Одностраничный сайт с галереей.",
-    image: '/images/2.png',
+    image: '/images/2.jpg',
     tags: ['React', 'JavaScript'],
     source: 'https://boring-pasteur-b2a288.netlify.app/',
     visit: 'https://boring-pasteur-b2a288.netlify.app/',
+    id: 1,
+  },
+  {
+    title: 'Bro | Wheels',
+    description:"Лэндинг компании, предоставляющей услуги подбора, покупки и сопровождения авто из США.",
+    image: '/images/3.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://kind-haibt-f0b03b.netlify.app/',
+    visit: 'https://kind-haibt-f0b03b.netlify.app/',
     id: 1,
   },
 
